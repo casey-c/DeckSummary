eeb3b686c8f2fb9aa378403f76ecdaab704b0cc9
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2021-05-08 13:46:12 -0400

    Added the Expression interface to represent syntax tree elements.

---
+++ src/main/java/DeckSummary/statictics/ast/Expression.java
@@ -0,0 +1,25 @@
+package DeckSummary.statictics.ast;
+
+import com.google.gson.*;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+import java.lang.reflect.Type;
+
+public interface Expression {
+    int evaluate(AbstractCard card);
+
+    class Deserializer implements JsonDeserializer<Expression> {
+        @Override
+        public Expression deserialize(JsonElement jsonElement, Type _type, JsonDeserializationContext context)
+                throws JsonParseException {
+            if (jsonElement.isJsonObject()) {
+                JsonObject object = jsonElement.getAsJsonObject();
+                // if it contains an operator and values, deserialise it as an operation.
+                if (object.has("operator") && object.has("values")) {
+                    return context.deserialize(object, Operation.class);
+                }
+            }
+            return context.deserialize(jsonElement, StatValue.class);
+        }
+    }
+}
